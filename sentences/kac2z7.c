05be05697de7209fa62775a42fcce1506b311667
kac2z7 <kac2z7@mail.missouri.edu>
2016-09-13 13:47:00 -0500

    Create kac2z7.c

---
+++ sentences/kac2z7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int kac2z7Sentence1(){
+  printf("People have been reporting strange things in the sky for thousands of years.\n");
+  return 1;
+}
+
+int kac2z7Sentence2(){
+  printf("Some of the sightings were of monsters.\n");
+  return 1;
+}
